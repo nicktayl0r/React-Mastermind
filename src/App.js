d9d2474959bd80e5a9fa12730edaf8e63d8c8ea3
Nick Taylor <nicholasjt22@gmail.com>
2018-02-13 10:35:48 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          Welcome to React Mastermind
+        </header>
+    );
+  }
+}
+
+export default App;
